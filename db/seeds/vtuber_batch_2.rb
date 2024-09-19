05df0f8ad6ac37910d2200bc8a5bcee397b2d5b6
Sunjun Hwang <sunjun.software@gmail.com>
2024-09-19 23:32:22 +0900

    vtuber batch 2 seeds

--- db/seeds/vtuber_batch_2.rb
+++ db/seeds/vtuber_batch_2.rb
@@ -8,6 +8,7 @@ require "open-uri"
 indie = Agency.find_by_name("Indie")
 puts indie.name
 pixellink = Agency.find_by_name("PixelLink")
+holostars = Agency.find_by_name("Holostars")
 
 if (!Vtuber.find_by_name("Non Anon"))
   non = Vtuber.create!(
@@ -37,13 +38,54 @@ end
     birthday: "2020/02/03",
     debut_date: "2023/06/16",
     yt_channel: "https://www.youtube.com/@ChiakiKatsumi",
+  },
+  {
+    name: "Miuna Usako",
+    jp_name: "みうな兎子",
+    description: "A once ordinary toy bunny brought magically back to life by an unknown force. Miuna is sure to win you over with her irresistible cuteness.",
+    agency: pixellink,
+    gender: "female",
+    main_language: "English",
+    birthday: "2020/12/07",
+    debut_date: "2023/06/16",
+    yt_channel: "https://www.youtube.com/@MiunaUsako",
+  },
+  {
+    name: "Nijyuna",
+    jp_name: "にじゅな",
+    description: "A singing rabbit born on the moon.",
+    agency: indie,
+    gender: "female",
+    main_language: "Japanese",
+    debut_date: "2023/01/07",
+    yt_channel: "https://www.youtube.com/@Nijyuuu7",
+    twitch_channel: "twitch.tv/nijyuna"
+  },
+  {
+    name: "Vesper Noir",
+    jp_name: "ノワール・ヴェスパー",
+    description: "It's his childish nature that allows him to speak so freely about his dreams. He might even actually let everyone around him dream.\nAdventurer's Guild TEMPUS's academic advisor.\nHe also holds the posts of custodian and arch-scholar at the Grand Library.\nLogical and rational at all times, he doesn't lift a finger unless completely convinced by something.\nHe has an aversion to sunlight due to spending too long cooped up, absorbed in his research.\n(Source: Holostars website)",
+    agency: holostars,
+    gender: "male",
+    main_language: "English",
+    birthday: "2020/02/13",
+    debut_date: "2022/07/24",
+    yt_channel: "https://www.youtube.com/@NoirVesper",
+    active: false
+  },
+  {
+    name: "Josuiji Shinri",
+    jp_name: "定水寺シンリ",
+    description: "A ronin who is part of Adventurer's Guild TEMPUS.\nHas the appearance of a wandering samurai, but was widely renowned as a master of the longbow.\nSo famed was his prowess, they would say he could use anything as an arrow and yet never fail to hit his mark.\nHe went into isolation for a while, lamenting the futility of war,\nbut took up his bow and arrow once again after witnessing the young men fighting a pack of Corruption Beasts.\n(Source: Holostars website)",
+    agency: holostars,
+    gender: "male",
+    main_language: "English",
+    birthday: "2020/06/23",
+    debut_date: "2023/01/08",
+    yt_channel: "https://www.youtube.com/@JosuijiShinri"
   }
-]
-
-
-if chiaki = Vtuber.find_by_name("Chiaki Katsumi")
-  ["singer"].each do |tag|
-    chiaki.add_tag(tag)
-    puts "Tags added to Chiaki"
+].each do |vtuber|
+  if !Vtuber.find_by_name(vtuber[:name])
+    puts "#{vtuber[:name]} created" if Vtuber.create!(vtuber)
   end
 end
