4ee9d57f09ae0d08d3fc5cac24122b0b5d9f8dc8
Sunjun Hwang <sunjun.software@gmail.com>
2024-09-24 20:29:57 +0900

    vtuber show form

--- db/seeds/agency_names.rb
+++ db/seeds/agency_names.rb
@@ -6,3 +6,5 @@
 @vebop = Agency.find_by_name("Vebop Project")
 @varium = Agency.find_by_name("Varium")
 @v4mirai = Agency.find_by_name("V4Mirai")
+@vdere = Agency.find_by_name("V-Dere")
+@phase = Agency.find_by_name("Phase Connect")
