2078350a8f1dc8310ee7fc542541130ccb0042c0
Sunjun Hwang <sunjun.software@gmail.com>
2024-11-12 01:32:50 +0900

    added password

---
+++ source/javascripts/controllers/password_controller.js
@@ -0,0 +1,10 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+
+  connect() {
+    // TODO: console.log something!
+    console.log("yatsuhashi");
+  }
+
+}
--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,32 @@
 // This is where it all goes :)
+// import "controllers"
+
+// // Eager load all controllers defined in the import map under controllers/**/*_controller
+// import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+// eagerLoadControllersFrom("controllers", application)
+
+// import { Application } from "@hotwired/stimulus";
+
+// import EventListenerController from "./controllers/event_listener_controller.js";
+
+// window.Stimulus = Application.start();
+// Stimulus.register("event-listener", EventListenerController);
+
+console.log("misokatsu");
+
+const password = document.querySelector("#password");
+// const enter_button = document.getElementById("enter-button");
+const enter_button = document.querySelector("#enter-button");
+
+const number = document.querySelector("#number");
+number.innerText = 5;
+
+// if(enter_button) {
+//   console.log("enter button");
+// }
+
+enter_button.addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log(event);
+  console.log(event.currentTarget);
+});
