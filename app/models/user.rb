2623538a12bc048520d1a382b15db23777630c41
Sunjun Hwang <sunjun.software@gmail.com>
2024-12-15 03:26:49 +0900

    lists can be reordered

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,7 @@ class User < ApplicationRecord
   end
 
   def lists
-    List.where(user_id: id)
+    List.where(user_id: id).order("order_num ASC")
   end
 
   def next_num
@@ -41,4 +41,22 @@ class User < ApplicationRecord
     User.where(username: name).first
   end
 
+  def move_list(old_num, new_num)
+    if old_num != new_num && old_num > 0 && old_num <= lists.length && new_num > 0 && new_num <= lists.length
+
+      target_list = lists[old_num - 1]
+
+      if new_num < old_num
+        lists[new_num - 1..old_num - 2].each do |list|
+          list.update(order_num: list.order_num + 1)
+        end
+      elsif new_num > old_num
+        lists[old_num..new_num - 1].each do |list|
+          list.update(order_num: list.order_num - 1)
+        end
+      end
+      target_list.update(order_num: new_num)
+
+    end
+  end
 end
