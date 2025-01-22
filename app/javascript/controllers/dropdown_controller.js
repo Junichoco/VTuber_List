64102cbd3dff140efdc28bf67bbcea08339613dd
Sunjun Hwang <sunjun.software@gmail.com>
2025-01-23 08:48:51 +0900

    navbar is responsive

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -3,17 +3,17 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["button", "menu"]
   connect() {
-    console.log("dropdown controller connected");
+    // console.log("dropdown controller connected");
   }
   toggle(event) {
     console.log("dropdown toggled");
     // this.menuTarget.classList.toggle("display");
     if(this.menuTarget.classList.contains("d-none")){
       this.menuTarget.classList.remove("d-none");
-      console.log("toggle on");
+      // console.log("toggle on");
     }else{
       this.menuTarget.classList.add("d-none");
-      console.log("toggle off");
+      // console.log("toggle off");
     }
   }
 }
